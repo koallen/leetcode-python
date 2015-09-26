fd52610e82a8f8129eba8417c09d25b56201be3b
Liu Siyuan <weifengzi2009@gmail.com>
2015-09-27 02:21:42 +0800

    Create 242_valid_anagram.py

---
+++ 242_valid_anagram.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s = ''.join(sorted(s))
+        t = ''.join(sorted(t))
+        if s == t:
+            return True
+        else:
+            return False
